e50b538c59242fc12f3917752e4a387aa9a9dc27
Hachuri <apinan.a@ctibkk.com>
2015-04-07 17:01:09 +0700

    Update Form1

--- TestApp/TestApp/Form1.Designer.cs
+++ TestApp/TestApp/Form1.Designer.cs
@@ -28,13 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.buttonX1 = new DevComponents.DotNetBar.ButtonX();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // buttonX1
+            // 
+            this.buttonX1.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.buttonX1.Location = new System.Drawing.Point(0, 0);
+            this.buttonX1.Name = "buttonX1";
+            this.buttonX1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(394, 296);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -42,6 +61,9 @@
         }
 
         #endregion
+
+        private DevComponents.DotNetBar.ButtonX buttonX1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
